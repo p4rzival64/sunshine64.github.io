6f55f618e03860b19f92803705afebb5e4fb1db0
p4rzival64 <60823802+p4rzival64@users.noreply.github.com>
2021-12-06 20:21:52 +0700

    Update myJs.js

--- sunshine/js/myJs.js
+++ sunshine/js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "He luu Hà Poodleeeee!",
+  text2: "T có điều này muốn hỏi m, m nhớ phải trả lời thật lòng nhé.",
+  text3: "T thích m, nhân dịp mùa Đông năm nay, m làm bạn gái t nhé?",
+  text4: "Nếu m ko trả lời mà thoát ra tức là đồng ý nha :v",
+  text5: "Khồng!!M mơ à???",
+  text6: "Đương nhiên rồii <3",
+  text7: "Cho t biết lí do m thích t đi :vvvv",
+  text8: "Gửi cho t nào <3",
+  text9: "Vì t đẹp try, vẽ đẹp, nghệ sĩ vlllll",
+  text10: "T biết mà ^^ Yêu m rất nhiềuuuu",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Cảm ơn m vì đã đáp lại tình cảm của t nhé :v Còn giờ thì chờ gì nữa mà ko direct cho t đi nàooo",
   text12: "Okii lunn <3",
 };
 
